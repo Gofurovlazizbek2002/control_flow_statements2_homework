1e1acd1bf6c4714276805428ddc7353adc938634
Gofurovlazizbek2002 <lazizbekgofurov997@gmail.com>
2022-10-19 18:39:44 +0500

    if01.py

--- if01.py
+++ if01.py
@@ -8,4 +8,15 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+
+
+    if a>b and a>c:
+        print(a)
+    if b>a and b>c:
+        print(b)
+    if c>a and c>b:
+        print(c) 
+    return 0
+
+
+print(main(24,35,46))
