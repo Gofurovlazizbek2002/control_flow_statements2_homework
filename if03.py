497ad3a1a9b3be68a49b92d08accad967bf5ed0c
Gofurovlazizbek2002 <lazizbekgofurov997@gmail.com>
2022-10-19 21:16:09 +0500

    if03.py

--- if03.py
+++ if03.py
@@ -8,4 +8,16 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    
+    if a>b and b>c or c>b and b>a:
+        x=b
+    if b>a and a>c or c>a and a>b:
+        x = a
+    if a>c and c>b or b>c and c>a:
+        x = c
+
+    return x
+     
+print(main(21,17,13))
+
+     
